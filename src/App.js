cf9e370e352f026c52fe5acb1e74bfe5f918ec8d
Rick Fleischman <rickf71@gmail.com>
2022-12-22 13:29:13 -0600

    a

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Navbar, NavbarBrand, Nav, Container } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar bg="dark" fg="light" expand="md" variant="dark">
+        <Container className="App">
+          <NavbarBrand href="#">My App</NavbarBrand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="mr-auto">
+              <Nav.Link href="#">Home</Nav.Link>
+              <Nav.Link href="#">Resume</Nav.Link>
+              <Nav.Link href="#">Examples</Nav.Link>
+              <Nav.Link href="#">About</Nav.Link>
+              <Nav.Link href="#">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
       <header className="App-header">
         <h1>H3llo!</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+            <a href="https://paletton.com/#uid=63J1p0k7c9i9ioD9wiNc2i2fZlS">Paletton Color Link</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
