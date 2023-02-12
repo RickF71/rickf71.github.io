43805e99bdbdb1c330adf88e34339573484cfb97
Rick Fleischman <rickf71@gmail.com>
2023-02-11 21:04:45 -0600

    Resume overhaul, other smaller changes

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
-import { Navbar, NavbarBrand, Nav, Container } from 'react-bootstrap';
+import { Navbar, NavbarBrand, Nav, Container, Row, Col
+        , ThemeProvider } from 'react-bootstrap';
+import { Routes, Route } from 'react-router-dom';
+import { Resume } from './Resume';
+import { Resume2 } from './Resume2';
+import { Home } from './Home';
+import { Examples } from './Examples';
+import { About } from './About';
+import { Contact } from './Contact';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
+const theme = {
+  primary: '#00b8d4',
+  secondary: '#f50057'
+};
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Navbar bg="dark" fg="light" expand="md" variant="dark">
         <Container className="App">
-          <NavbarBrand href="#">My App</NavbarBrand>
+          <NavbarBrand href="#">Richard Fleischman</NavbarBrand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse>
             <Nav className="mr-auto">
-              <Nav.Link href="#">Home</Nav.Link>
-              <Nav.Link href="#">Resume</Nav.Link>
-              <Nav.Link href="#">Examples</Nav.Link>
-              <Nav.Link href="#">About</Nav.Link>
-              <Nav.Link href="#">Contact</Nav.Link>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/resume2">Resume</Nav.Link>
+              {/* <Nav.Link href="/resume2">Resume2</Nav.Link> */}
+              <Nav.Link href="/examples">Examples</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <header className="App-header">
-        <h1>H3llo!</h1>
-        
-        <p>
-            <a href="https://paletton.com/#uid=63J1p0k7c9i9ioD9wiNc2i2fZlS">Paletton Color Link</a>
-        </p>
-        
-      </header>
-    </div>
+
+
+      {/* Define Routes  */}
+      <Routes>
+        <Route path="/" element={ <Home/> } />
+        <Route path="/resume" element={ <Resume/> } />
+        <Route path="/resume2" element={ <Resume2/> } />
+        <Route path="/examples" element={ <Examples/> } />
+        <Route path="/about" element={ <About/> } />
+        <Route path="/contact" element={ <Contact/> } />
+      </Routes>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+export const Home = () => {
+    return (
+        <>
+            <Container>
+                <Row><h1>Richard Fleischman</h1></Row>
+                <p>
+                    Hello I am a computer programmer with a wide variety of experience
+                    in many aspects of Web based and other programming.
+                    In addition I've worked with many different database systems.
+                </p>
+                <p>
+                    <p>Written with several technologies</p>
+
+                    <ul>
+                        <li>React 18.2.0</li>
+                        <li>Bootstrap 5.2</li>
+                        <li>react-bootstrap v2.7.0</li>
+                    </ul>
+                </p>
+            </Container>
+
+
+        </>
+    )
+};
+  
+// export default Resume;
