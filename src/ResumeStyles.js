56cda51a69bd5a0485b1a86a7d0e86ab5db2bc55
Richard Fleischman <rickf71@gmail.com>
2023-03-04 04:41:54 -0600

    tiny

---
+++ src/ResumeStyles.js
@@ -0,0 +1,77 @@
+// ***** PAGE STYLES *****
+export const styles = {
+    summary: {
+        backgroundColor: '#565656',
+        color: 'white',
+        paddingLeft:0,
+        paddingRight:0,
+        paddingBottom:'0.5rem',
+    },
+    summarySection: {
+        backgroundColor:'#454545',
+        fontSize: '1.5rem',
+        padding:'0.25rem',
+        paddingLeft: '1rem',
+        marginTop: '1rem',
+        textShadow: '1px 1px 0 #fff, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #fff;',
+    },
+    summarySection2: {
+        fontSize: '1rem',
+        paddingLeft: '1rem',
+        marginTop: '1rem',
+        marginBottom: '0rem',
+        paddingBottom: 0,
+        textAlign:'center',
+        textShadow: '1px 1px 0 #fff, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #fff;',
+    },
+    summarySection3: {
+        fontSize: '1.25rem',
+        fontWeight: 'bold',
+        padding:'0.25rem',
+        textAlign:'center',
+        marginTop: '0rem',
+        paddingTop: 0,
+        textShadow: '1px 1px 0 #fff, -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #fff;',
+    },
+    experience: {
+        backgroundColor: '#d7ced7',
+        color: 'black',
+        height: '100%'
+      },
+    expTitle: {
+        textAlign: 'center',
+        marginTop:'.5rem',
+        padding:'.25rem',
+        fontSize:'2rem',
+        color: 'black',
+        fontWeight: 'bold',
+        background: "linear-gradient(0deg, #88a, #c09f80 .5rem)",
+        borderTopLeftRadius: '.5rem', 
+        borderTopRightRadius: '.5rem', 
+        border:'1rem',
+        textShadow: '1px 1px 3px rgba(0,0,0,0.5)',
+    }, 
+    expJobItem: {
+        backgroundColor: '#76323f', 
+        background: "linear-gradient(180deg, #88a, #d7ced7 5rem)",
+        borderLeft: '5px solid #88a',
+        fontSize: '1em',
+        marginBottom: '.5rem',
+        paddingLeft: '0.5rem',
+    },
+    expJobItemHead: {
+        fontSize: '1.1em',
+        textShadow: '1px 1px 3px rgba(0,0,0,0.5)',
+    },
+    expTag: {
+        borderColor: "black", 
+        marginRight:'.25rem',  
+        paddingLeft: '.2rem',
+        paddingRight: '.2rem',
+        fontSize:'0.9rem',
+        background: "linear-gradient(0deg, #aaa, #d7ced7 1.25rem)",
+        border: '1px solid #565656',
+        borderRadius: '.5rem', 
+    },
+
+  };
